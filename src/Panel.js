30936ea05c305564118f4b93cddbe664ecb04e08
Sayandip Halder <sayandiph4@gmail.com>
2022-01-27 22:17:53 +0530

    add panel

---
+++ src/Panel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Panel.css';
+
+function Panel() {
+  return(
+    <div className="control-panel">
+        <h3>Interactive GeoJSON</h3>
+    </div>
+  );
+}
+
+export default Panel;
